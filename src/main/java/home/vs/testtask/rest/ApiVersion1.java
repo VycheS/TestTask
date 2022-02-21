00ab223d78a0303072654e0871d4fc93da28882c
VycheS <lieber_freund@mail.ru>
2022-02-21 10:11:47 +0000

    добавление rest контроллера

---
+++ src/main/java/home/vs/testtask/rest/ApiVersion1.java
@@ -0,0 +1,5 @@
+package home.vs.testtask.rest;
+
+public interface ApiVersion1 {
+    public final static String VERSION = "v1";
+}
---
+++ src/main/java/home/vs/testtask/rest/UserRestController.java
@@ -0,0 +1,48 @@
+package home.vs.testtask.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import home.vs.testtask.model.User;
+import home.vs.testtask.service.UserService;
+
+@RestController
+public class UserRestController implements ApiVersion1 {
+    private UserService service;
+    
+    private final static String PATH = "/" + VERSION + "/users";
+
+    @Autowired
+    public UserRestController(UserService service) {
+        this.service = service;
+    }
+
+    @GetMapping(path = PATH, produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody List<User> getAll() {
+        return this.service.getAll();
+    }
+
+    @GetMapping(path = PATH + "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public @ResponseBody User getById(@PathVariable Long id) {
+        return this.service.getById(id);
+    }
+
+    @PostMapping(path = PATH)
+    public @ResponseBody User create(@RequestBody User user) {
+        return this.create(user);
+    }
+
+    @DeleteMapping(path = PATH + "/{id}")
+    public @ResponseBody User deleteById(@PathVariable Long id) {
+        return this.service.deleteById(id);
+    }
+}
