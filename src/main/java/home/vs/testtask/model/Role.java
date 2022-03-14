e5704f5ea912a91a7e613600ec03f8991e4e189b
VycheS <lieber_freund@mail.ru>
2022-03-14 19:56:56 +0900

    внедрение разрешения в роли с компонентом authority

--- src/main/java/home/vs/testtask/model/Role.java
+++ src/main/java/home/vs/testtask/model/Role.java
@@ -1,6 +1,28 @@
 package home.vs.testtask.model;
 
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
 public enum Role {
-    USER,
-    ADMIN
+    USER(Stream.of(Permission.USERS_READ).collect(Collectors.toSet())),
+    ADMIN(Stream.of(Permission.USERS_WRITE, Permission.USERS_WRITE).collect(Collectors.toSet()));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+   
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+            .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+            .collect(Collectors.toSet());
+    }
 }
