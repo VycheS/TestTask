0d6b1a3c444023db4df89407ffa6127330a3d32a
VycheS <lieber_freund@mail.ru>
2022-03-14 22:54:34 +0900

    переименование

--- src/main/java/home/vs/testtask/security/UserDetailsServiceImpl.java
+++ src/main/java/home/vs/testtask/security/UserDetailsServiceImpl.java
@@ -1,14 +1,29 @@
 package home.vs.testtask.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import home.vs.testtask.model.User;
+import home.vs.testtask.repository.UserRepository;
+
+@Service("userDetailsServiceImpl")
 public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    
+    @Autowired
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        // TODO Auto-generated method stub
+        User user = userRepository.findByLogin(username).orElseThrow(() -> {
+            return new UsernameNotFoundException("User doesn't exists");
+        });
         return null;
     }
     
